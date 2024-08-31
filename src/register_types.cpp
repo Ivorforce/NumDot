f34cced22245a35ec476f45b164f9ce1b93cfaaa
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-08-31 13:07:53 +0200

    Remove a few files that should not be tracked.

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -7,10 +7,11 @@
 #include <gdextension_interface.h>
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
 
 using namespace godot;
 
-void initialize_example_module(ModuleInitializationLevel p_level) {
+void initialize_numdot_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
@@ -19,7 +20,7 @@ void initialize_example_module(ModuleInitializationLevel p_level) {
 	GDREGISTER_CLASS(ND);
 }
 
-void uninitialize_example_module(ModuleInitializationLevel p_level) {
+void uninitialize_numdot_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
@@ -30,8 +31,8 @@ extern "C" {
 GDExtensionBool GDE_EXPORT example_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
 	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
-	init_obj.register_initializer(initialize_example_module);
-	init_obj.register_terminator(uninitialize_example_module);
+	init_obj.register_initializer(initialize_numdot_module);
+	init_obj.register_terminator(uninitialize_numdot_module);
 	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
 	return init_obj.init();

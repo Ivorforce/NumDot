e1d892e4e6818659a21b204f68d9cc462f169f23
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-09-12 14:59:14 +0200

    Fix trig function promotion.

--- src/vatensor/trigonometry.cpp
+++ src/vatensor/trigonometry.cpp
@@ -9,13 +9,13 @@
 using namespace va;
 
 VArray va::sin(const VArray &array) {
-    return xoperation<promote::common_type>(va::XFunction<xt::math::sin_fun> {}, array.to_compute_variant());
+    return xoperation<promote::function_result<xt::math::sin_fun>>(va::XFunction<xt::math::sin_fun> {}, array.to_compute_variant());
 }
 
 VArray va::cos(const VArray &array) {
-    return xoperation<promote::common_type>(va::XFunction<xt::math::cos_fun> {}, array.to_compute_variant());
+    return xoperation<promote::function_result<xt::math::cos_fun>>(va::XFunction<xt::math::cos_fun> {}, array.to_compute_variant());
 }
 
 VArray va::tan(const VArray &array) {
-    return xoperation<promote::common_type>(va::XFunction<xt::math::tan_fun> {}, array.to_compute_variant());
+    return xoperation<promote::function_result<xt::math::tan_fun>>(va::XFunction<xt::math::tan_fun> {}, array.to_compute_variant());
 }

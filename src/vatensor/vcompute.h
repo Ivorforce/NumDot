77a290f2afb4e2ced97777c3e019b2896e86826e
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-09-18 19:11:26 +0200

    Change to copying for arguments instead of casting. This decreases performance where casting is needed, but decreases binary size in half. Add a compile option NUMDOT_CAST_INSTEAD_OF_COPY_FOR_ARGUMENTS to return to the previous behavior.

--- src/vatensor/vcompute.h
+++ src/vatensor/vcompute.h
@@ -7,10 +7,19 @@ namespace va {
     template <typename NeededType, typename Type>
     auto promote_compute_case_if_needed(const compute_case<Type>& arg) {
         if constexpr (std::is_same_v<Type, NeededType>) {
+            // Most common situation: the argument we need is the same as the argument that's given.
             return arg;
         }
         else {
+            // Casting can considerably increase performance (from a small test, it was 25%).
+            // However, this is only relevant for operations that even need casting.
+            // The cost for casting instead of copying is a much larger binary size (100% increase).
+            // Most people will probably prefer the small binary, and accept less optimized wrong dtype operations.
+#ifdef NUMDOT_CAST_INSTEAD_OF_COPY_FOR_ARGUMENTS
             return xt::cast<NeededType>(arg);
+#else
+            return xt::xarray<NeededType>(arg);
+#endif
         }
     }
 

38ad34941940e4a24c64d4389a9b5442860d0710
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-09-23 01:33:55 +0200

    Implemented lots of leaf interpretations for nd.array([...]). It is now much more versatile in interpreting types hidden deep in arrays. Fixes #65.

--- src/gdconvert/conversion_array.cpp
+++ src/gdconvert/conversion_array.cpp
@@ -35,97 +35,6 @@
 #include "xtensor/xtensor_forward.hpp"                 // for xarray
 #include "xtl/xiterator_base.hpp"                      // for operator+
 
-
-va::VArray array_as_varray(const Array& array) {
-    va::shape_type shape;
-    va::DType dtype = va::DTypeMax;
-    std::vector<Array> current_dim_arrays = { array };
-
-    while (true) {
-        size_t current_dim_size = current_dim_arrays[0].size();
-
-        for (const auto& array : current_dim_arrays) {
-            // Sizes are the same.
-            if (current_dim_size == array.size()) { continue; }
-
-            // We defined the size before, this element can broadcast.
-            if (array.size() == 1 && current_dim_size >= 1) { continue; }
-
-            // We broadcasted before, this element defines the size.
-            if (current_dim_size == 1 && array.size() > 1) {
-                current_dim_size = array.size();
-                continue;
-            }
-
-            throw std::runtime_error("array has an inhomogenous shape");
-        }
-
-        shape.push_back(current_dim_size);
-        std::vector<Array> next_dim_arrays;
-
-        for (const Array& array : current_dim_arrays) {
-            for (int i = 0; i < array.size(); ++i) {
-                const Variant& element = array[i];
-                switch (element.get_type()) {
-                    case Variant::ARRAY:
-                        next_dim_arrays.push_back(element);
-                        break;
-                    case Variant::FLOAT:
-                        dtype = va::dtype_common_type(dtype, va::Float64);
-                        break;
-                    case Variant::INT:
-                        dtype = va::dtype_common_type(dtype, va::Int64);
-                        break;
-                    case Variant::BOOL:
-                        dtype = va::dtype_common_type(dtype, va::Bool);
-                        break;
-                    default:
-                        throw std::runtime_error("unsupported array type");
-                }
-            }
-        }
-
-        if (next_dim_arrays.empty()) break;
-
-        current_dim_arrays = std::move(next_dim_arrays);
-    }
-
-    if (dtype == va::DTypeMax) dtype = va::Float64; // Default dtype
-
-    va::VArray varray = va::empty(dtype, shape);
-    std::vector<std::tuple<xt::xstrided_slice_vector, Array>> next = { std::make_tuple(xt::xstrided_slice_vector {}, array) };
-
-    while (!next.empty()) {
-        const auto [array_base_idx, array] = std::move(next.back());
-        next.pop_back();
-
-        for (size_t i = 0; i < array.size(); ++i) {
-            auto element_idx = array_base_idx;
-            element_idx.emplace_back(i);
-
-            const auto& array_element = array[i];
-            switch (array_element.get_type()) {
-                case Variant::ARRAY:
-                    next.push_back({element_idx, static_cast<Array>(array_element)});
-                    break;
-                case Variant::FLOAT:
-                    varray.slice(element_idx).fill(static_cast<double_t>(array_element));
-                    break;
-                case Variant::INT:
-                    varray.slice(element_idx).fill(static_cast<int64_t>(array_element));
-                    break;
-                case Variant::BOOL:
-                    varray.slice(element_idx).fill(static_cast<bool>(array_element));
-                    break;
-                default:
-                    throw std::runtime_error("unsupported array type");
-            }
-        }
-    }
-
-    return varray;
-}
-
 template <typename C, typename T>
 va::VArray packed_as_xarray(const T shape_array) {
     auto size = static_cast<std::size_t>(shape_array.size());
@@ -139,13 +48,271 @@ va::VArray packed_as_xarray(const T shape_array) {
     return va::from_store(store);
 }
 
+void add_size_at_idx(va::shape_type& shape, const std::size_t idx, const std::size_t value) {
+    if (shape.size() > idx) {
+        auto current_dim_size = shape[idx];
+
+        // Sizes are the same.
+        if (current_dim_size == value) return;
+
+        // We defined the size before, this element can broadcast.
+        if (value == 1 && current_dim_size >= 1) return;
+
+        // We broadcasted before, this element defines the size.
+        if (current_dim_size == 1 && value > 1) {
+            shape[idx] = value;
+            return;
+        }
+
+        throw std::runtime_error("array has an inhomogenous shape");
+    }
+    else if (shape.size() == idx) {
+        shape.push_back(value);
+    }
+    else {
+        throw std::invalid_argument("index out of range");
+    }
+}
+
+va::VArray array_as_varray(const Array& input_array) {
+    va::shape_type shape;
+    va::DType dtype = va::DTypeMax;
+    std::vector<Array> current_dim_arrays = { input_array };
+
+    for (size_t current_dim_idx = 0; true; ++current_dim_idx) {
+        for (const auto& array : current_dim_arrays) {
+            add_size_at_idx(shape, current_dim_idx, array.size());
+        }
+
+        std::vector<Array> next_dim_arrays;
+        for (const Array& array : current_dim_arrays) {
+            for (int i = 0; i < array.size(); ++i) {
+                const Variant& array_element = array[i];
+
+                switch (array_element.get_type()) {
+                    case Variant::OBJECT: {
+                        if (const auto ndarray = Object::cast_to<NDArray>(array_element)) {
+                            auto& varray = ndarray->array;
+                            auto varray_dim_idx = current_dim_idx;
+                            for (auto size : varray.shape) {
+                                add_size_at_idx(shape, varray_dim_idx, size);
+                                varray_dim_idx++;
+                            }
+                            continue;
+                        }
+                    }
+                    case Variant::ARRAY:
+                        next_dim_arrays.push_back(array_element);
+                        continue;
+                    case Variant::BOOL:
+                        dtype = va::dtype_common_type(dtype, va::Bool);
+                        continue;
+                    case Variant::INT:
+                        dtype = va::dtype_common_type(dtype, va::Int64);
+                        continue;
+                    case Variant::FLOAT:
+                        dtype = va::dtype_common_type(dtype, va::Float64);
+                        continue;
+                    case Variant::PACKED_BYTE_ARRAY: {
+                        dtype = va::dtype_common_type(dtype, va::variant_to_dtype(uint8_t()));
+                        PackedByteArray packed = array_element;
+                        add_size_at_idx(shape, current_dim_idx + 1, packed.size());
+                        continue;
+                    }
+                    case Variant::PACKED_INT32_ARRAY: {
+                        dtype = va::dtype_common_type(dtype, va::variant_to_dtype(int32_t()));
+                        PackedInt32Array packed = array_element;
+                        add_size_at_idx(shape, current_dim_idx + 1, packed.size());
+                        continue;
+                    }
+                    case Variant::PACKED_INT64_ARRAY: {
+                        dtype = va::dtype_common_type(dtype, va::variant_to_dtype(int64_t()));
+                        PackedInt64Array packed = array_element;
+                        add_size_at_idx(shape, current_dim_idx + 1, packed.size());
+                        continue;
+                    }
+                    case Variant::PACKED_FLOAT32_ARRAY: {
+                        dtype = va::dtype_common_type(dtype, va::variant_to_dtype(float_t()));
+                        PackedFloat32Array packed = array_element;
+                        add_size_at_idx(shape, current_dim_idx + 1, packed.size());
+                        continue;
+                    }
+                    case Variant::PACKED_FLOAT64_ARRAY: {
+                        dtype = va::dtype_common_type(dtype, va::variant_to_dtype(double_t()));
+                        PackedFloat64Array packed = array_element;
+                        add_size_at_idx(shape, current_dim_idx + 1, packed.size());
+                        continue;
+                    }
+                    case Variant::VECTOR2I:
+                        dtype = va::dtype_common_type(dtype, va::variant_to_dtype(int64_t()));
+                        add_size_at_idx(shape, current_dim_idx + 1, 2);
+                        continue;
+                    case Variant::VECTOR3I:
+                        dtype = va::dtype_common_type(dtype, va::variant_to_dtype(int64_t()));
+                        add_size_at_idx(shape, current_dim_idx + 1, 3);
+                        continue;
+                    case Variant::VECTOR4I:
+                        dtype = va::dtype_common_type(dtype, va::variant_to_dtype(int64_t()));
+                        add_size_at_idx(shape, current_dim_idx + 1, 4);
+                        continue;
+                    case Variant::VECTOR2:
+                        dtype = va::dtype_common_type(dtype, va::variant_to_dtype(real_t()));
+                        add_size_at_idx(shape, current_dim_idx + 1, 2);
+                        continue;
+                    case Variant::VECTOR3:
+                        dtype = va::dtype_common_type(dtype, va::variant_to_dtype(real_t()));
+                        add_size_at_idx(shape, current_dim_idx + 1, 3);
+                        continue;
+                    case Variant::VECTOR4:
+                        dtype = va::dtype_common_type(dtype, va::variant_to_dtype(real_t()));
+                        add_size_at_idx(shape, current_dim_idx + 1, 4);
+                        continue;
+                    default:
+                        break;
+                }
+
+                throw std::runtime_error("unsupported array type");
+            }
+        }
+
+        if (next_dim_arrays.empty()) break;
+
+        current_dim_arrays = std::move(next_dim_arrays);
+    }
+
+    if (dtype == va::DTypeMax) dtype = va::Float64; // Default dtype
+
+    va::VArray varray = va::empty(dtype, shape);
+    std::vector<std::tuple<xt::xstrided_slice_vector, Array>> next = { std::make_tuple(xt::xstrided_slice_vector {}, input_array) };
+
+    while (!next.empty()) {
+        const auto [array_base_idx, array] = std::move(next.back());
+        next.pop_back();
+
+        for (size_t i = 0; i < array.size(); ++i) {
+            auto element_idx = array_base_idx;
+            element_idx.emplace_back(i);
+
+            const auto& array_element = array[i];
+            switch (array_element.get_type()) {
+                case Variant::OBJECT: {
+                    if (const auto ndarray = Object::cast_to<NDArray>(array_element)) {
+                        varray.slice(element_idx).set_with_array(ndarray->array);
+                        continue;
+                    }
+                }
+                case Variant::ARRAY:
+                    next.push_back({element_idx, static_cast<Array>(array_element)});
+                    continue;
+                case Variant::BOOL:
+                    // TODO We should use element assign rather than slice - fill for all these.
+                    varray.slice(element_idx).fill(static_cast<bool>(array_element));
+                    continue;
+                case Variant::INT:
+                    varray.slice(element_idx).fill(static_cast<int64_t>(array_element));
+                    continue;
+                case Variant::FLOAT:
+                    varray.slice(element_idx).fill(static_cast<double_t>(array_element));
+                    continue;
+                case Variant::PACKED_BYTE_ARRAY: {
+                    varray.slice(element_idx).set_with_array(packed_as_xarray<uint8_t>(PackedByteArray(array_element)));
+                    continue;
+                }
+                case Variant::PACKED_INT32_ARRAY: {
+                    varray.slice(element_idx).set_with_array(packed_as_xarray<uint8_t>(PackedInt32Array(array_element)));
+                    continue;
+                }
+                case Variant::PACKED_INT64_ARRAY: {
+                    varray.slice(element_idx).set_with_array(packed_as_xarray<uint8_t>(PackedInt64Array(array_element)));
+                    continue;
+                }
+                case Variant::PACKED_FLOAT32_ARRAY: {
+                    varray.slice(element_idx).set_with_array(packed_as_xarray<uint8_t>(PackedFloat32Array(array_element)));
+                    continue;
+                }
+                case Variant::PACKED_FLOAT64_ARRAY: {
+                    varray.slice(element_idx).set_with_array(packed_as_xarray<uint8_t>(PackedFloat64Array(array_element)));
+                    continue;
+                }
+                case Variant::VECTOR2I: {
+                    const Vector2i vector = array_element;
+                    element_idx.emplace_back(0);
+                    varray.slice(element_idx).fill(vector.x);
+                    element_idx.back() = 1;
+                    varray.slice(element_idx).fill(vector.y);
+                    continue;
+                }
+                case Variant::VECTOR3I: {
+                    const Vector3i vector = array_element;
+                    element_idx.emplace_back(0);
+                    varray.slice(element_idx).fill(vector.x);
+                    element_idx.back() = 1;
+                    varray.slice(element_idx).fill(vector.y);
+                    element_idx.back() = 2;
+                    varray.slice(element_idx).fill(vector.z);
+                    continue;
+                }
+                case Variant::VECTOR4I: {
+                    const Vector4i vector = array_element;
+                    element_idx.emplace_back(0);
+                    varray.slice(element_idx).fill(vector.x);
+                    element_idx.back() = 1;
+                    varray.slice(element_idx).fill(vector.y);
+                    element_idx.back() = 2;
+                    varray.slice(element_idx).fill(vector.z);
+                    element_idx.back() = 3;
+                    varray.slice(element_idx).fill(vector.w);
+                    continue;
+                }
+                case Variant::VECTOR2: {
+                    const Vector2 vector = array_element;
+                    element_idx.emplace_back(0);
+                    varray.slice(element_idx).fill(vector.x);
+                    element_idx.back() = 1;
+                    varray.slice(element_idx).fill(vector.y);
+                    continue;
+                }
+                case Variant::VECTOR3: {
+                    const Vector3 vector = array_element;
+                    element_idx.emplace_back(0);
+                    varray.slice(element_idx).fill(vector.x);
+                    element_idx.back() = 1;
+                    varray.slice(element_idx).fill(vector.y);
+                    element_idx.back() = 2;
+                    varray.slice(element_idx).fill(vector.z);
+                    continue;
+                }
+                case Variant::VECTOR4: {
+                    const Vector4 vector = array_element;
+                    element_idx.emplace_back(0);
+                    varray.slice(element_idx).fill(vector.x);
+                    element_idx.back() = 1;
+                    varray.slice(element_idx).fill(vector.y);
+                    element_idx.back() = 2;
+                    varray.slice(element_idx).fill(vector.z);
+                    element_idx.back() = 3;
+                    varray.slice(element_idx).fill(vector.w);
+                    continue;
+                }
+                default:
+                    break;
+            }
+
+            throw std::runtime_error("unsupported array type");
+        }
+    }
+
+    return varray;
+}
+
 va::VArray variant_as_array(const Variant& array) {
     switch (array.get_type()) {
-        case Variant::OBJECT:
-            if (auto ndarray = Object::cast_to<NDArray>(array)) {
+        case Variant::OBJECT: {
+            if (const auto ndarray = Object::cast_to<NDArray>(array)) {
                 return ndarray->array;
             }
             break;
+        }
         case Variant::ARRAY: {
             return array_as_varray(array);
         }

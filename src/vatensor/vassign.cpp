90a2d83d36a98f20e48dfb889f095eb66ea344c7
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-09-23 17:00:11 +0200

    Add broadcasting_assign, to avoid having to xt::view into the compute variants.

--- src/vatensor/vassign.cpp
+++ src/vatensor/vassign.cpp
@@ -1,17 +1,21 @@
 #include "vassign.h"
-#include "xtensor/xview.hpp"
+#include "xtensor/xassign.hpp"
 
 using namespace va;
 
-// TODO xt::view is needed because xarray_adaptor is container_semantic,
-//  which is expected to resize on assignments.
-// We should find a better solution...
+// computed_assign on containers doesn't assign data, it tries to assign to the whole container.
+// This is basically view_semantic's computed_assign.
+template <typename T, typename E>
+inline void broadcasting_assign(xt::xexpression<T>& t, const xt::xexpression<E>& e) {
+    xt::assert_compatible_shape(t, e);
+    xt::assign_data(t, e, xt::detail::get_rhs_triviality(e.derived_cast()));
+}
 
 void va::assign(ComputeVariant& array, const ComputeVariant& value) {
     std::visit([](auto& carray, const auto& cvalue) {
         using T = typename std::decay_t<decltype(carray)>::value_type;
         // Cast first to reduce number of combinations down the line.
-        xt::view(carray, xt::all()).computed_assign(xt::cast<T>(cvalue));
+        broadcasting_assign(carray, xt::cast<T>(cvalue));
     }, array, value);
 }
 
@@ -19,7 +23,7 @@ void va::assign(ComputeVariant& array, const ArrayVariant& value) {
     std::visit([](auto& carray, const auto& cvalue) {
         using T = typename std::decay_t<decltype(carray)>::value_type;
         // Cast first to reduce number of combinations down the line.
-        xt::view(carray, xt::all()).assign_xexpression(xt::cast<T>(cvalue));
+        broadcasting_assign(carray, xt::cast<T>(cvalue));
     }, array, value);
 }
 

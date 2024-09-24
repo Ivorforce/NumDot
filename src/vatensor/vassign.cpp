31c8559293fa09b9485cdb3cc8d670c225d7905f
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-09-24 22:25:07 +0200

    Don't cast before assignment. By now, this doesn't change performance much.

--- src/vatensor/vassign.cpp
+++ src/vatensor/vassign.cpp
@@ -5,25 +5,19 @@ using namespace va;
 
 void va::assign(ComputeVariant& array, const ComputeVariant& value) {
     std::visit([](auto& carray, const auto& cvalue) {
-        using T = typename std::decay_t<decltype(carray)>::value_type;
-        // Cast first to reduce number of combinations down the line.
-        broadcasting_assign(carray, xt::cast<T>(cvalue));
+        broadcasting_assign(carray, cvalue);
     }, array, value);
 }
 
 void va::assign_nonoverlapping(ComputeVariant& array, const ArrayVariant& value) {
     std::visit([](auto& carray, const auto& cvalue) {
-        using T = typename std::decay_t<decltype(carray)>::value_type;
-        // Cast first to reduce number of combinations down the line.
-        broadcasting_assign(carray, xt::cast<T>(cvalue));
+        broadcasting_assign(carray, cvalue);
     }, array, value);
 }
 
 void va::assign(ComputeVariant& array, VConstant value) {
     std::visit([](auto& carray, const auto cvalue) {
-        // Cast first to reduce number of combinations down the line.
-        using T = typename std::decay_t<decltype(carray)>::value_type;
-        carray.fill(static_cast<T>(cvalue));
+        carray.fill(cvalue);
     }, array, value);
 }
 

36d6619a73bdb0eca3ddc2ba3250b487dd304a5f
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-09-24 20:13:39 +0200

    Make variant_to_axes not use godot auto conversion.

--- src/gdconvert/conversion_axes.cpp
+++ src/gdconvert/conversion_axes.cpp
@@ -19,13 +19,19 @@ va::Axes variant_to_axes(const Variant& variant) {
             return packed_as_array<std::vector<std::ptrdiff_t>>(PackedInt32Array(variant));
         case Variant::PACKED_INT64_ARRAY:
             return packed_as_array<std::vector<std::ptrdiff_t>>(PackedInt64Array(variant));
+        case Variant::ARRAY: {
+            const Array axes_array = variant;
+            auto axes = va::Axes(axes_array.size());
+            for (int64_t i = 0; i < axes_array.size(); i++) {
+                const Variant& element = axes_array[i];
+                if (element.get_type() != Variant::INT)
+                    throw std::runtime_error("Axis must be an integer");
+                axes[i] = static_cast<int64_t>(element);
+            }
+            return axes;
+        }
         default:
             break;
-    }
-
-    // TODO Godot will probably convert float to int. We should check.
-    if (Variant::can_convert(type, Variant::Type::PACKED_INT32_ARRAY)) {
-        return packed_as_array<std::vector<std::ptrdiff_t>>(PackedInt32Array(variant));
     }
 
     throw std::runtime_error("Variant cannot be converted to a shape.");

4c4f955c18bec4da71ac50d0aa0565b2148a222b
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-10-06 19:52:34 +0200

    Add a scalar math benchmark.

--- demo/benchmarks/benchmarks.gd
+++ demo/benchmarks/benchmarks.gd
@@ -4,5 +4,6 @@ func _ready() -> void:
 	$SieveOfEratosthenes.run_benchmark()
 	$GeneratePolygon.run_benchmark()
 	$Math.run_benchmark()
+	$ScalarMath.run_benchmark()
 	$Reductions.run_benchmark()
 	$Matrix.run_benchmark()

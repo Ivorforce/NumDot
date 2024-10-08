8cc469082f58ac49b5d55e0c90a8a477c9521fd9
Akshay <sakshays.2000@gmail.com>
2024-10-09 00:38:07 +0200

    detect input via inputmap instead of keycode (missed commit)

--- demo/demos/launcher/input_listener.gd
+++ demo/demos/launcher/input_listener.gd
@@ -2,6 +2,6 @@ extends Node
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
-		if event.keycode == KEY_B:
+		if event.is_action_pressed("back"):
 			TransitionManager.change_scene("res://demos/launcher/launcher.tscn")
 	

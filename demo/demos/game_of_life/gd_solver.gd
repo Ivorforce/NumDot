bf17dbb3e44507837f42c921ea3792f453627abf
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-10-10 23:51:04 +0200

    Use a shader for the game of life visuals. Remove the grid outer bounds from the drawn area; that shouldn't be part of the grid.

--- demo/demos/game_of_life/gd_solver.gd
+++ demo/demos/game_of_life/gd_solver.gd
@@ -6,17 +6,21 @@ var image_data: PackedByteArray
 
 func initialize() -> void:
 	grid_front = PackedByteArray()
-	grid_front.resize(params.N * params.N)
+	
+	# Need one row / column on each side extra so the lookup later is easier
+	grid_front.resize((params.N + 2) * (params.N + 2))
 	grid_back = grid_front.duplicate()
-	image_data = grid_front.duplicate()
+	
+	image_data = PackedByteArray()
+	image_data.resize(params.N * params.N)
 	place_random()
 
 func to_idx(x: int, y: int) -> int:
-	return x + params.N * y
+	return x + (params.N + 2) * y
 
 func simulation_step() -> void:
-	for i in range(1, params.N-1):
-		for j in range(1, params.N-1):
+	for i in range(1, params.N + 1):
+		for j in range(1, params.N + 1):
 			var neighbor_count := grid_front[to_idx(i - 1, j - 1)] + grid_front[to_idx(i, j - 1)] + grid_front[to_idx(i + 1, j - 1)]\
 				+ grid_front[to_idx(i - 1, j)] + grid_front[to_idx(i + 1, j)]\
 				+ grid_front[to_idx(i - 1, j + 1)] + grid_front[to_idx(i, j + 1)] + grid_front[to_idx(i + 1, j + 1)]
@@ -32,12 +36,14 @@ func simulation_step() -> void:
 	grid_back = tmp
 
 func on_draw() -> void:
-	for i in image_data.size():
-		image_data[i] = grid_front[i] * 255
+	for i in range(0, params.N):
+		for j in range(0, params.N):
+			image_data[i + params.N * j] = grid_front[to_idx(i + 1, j + 1)]
+	
 	params._image.set_data(params.N, params.N, false, Image.FORMAT_R8, image_data)
 	params.update_texture()
 
 func place_random() -> void:
-	for x in range(params.N - 2):
-		for y in range(params.N - 2):
-			grid_front[to_idx(x + 1, y + 1)] = randi() % 2
+	for i in range(1, params.N + 1):
+		for j in range(1, params.N + 1):
+			grid_front[to_idx(i, j)] = randi() % 2
--- demo/demos/game_of_life/nd_solver.gd
+++ demo/demos/game_of_life/nd_solver.gd
@@ -2,6 +2,7 @@ extends GameOfLifeSolver
 
 var is_alive: NDArray
 var is_alive_inner: NDArray
+var image_data: NDArray
 var neighour_count: NDArray
 var neighbor_kernel: NDArray
 var tmp_inner: NDArray
@@ -9,10 +10,11 @@ var tmp_inner: NDArray
 var rng := nd.default_rng()
 
 func initialize() -> void:
-	var grid_size := [params.N, params.N]
+	var grid_size := [params.N + 2, params.N + 2]
 	
 	is_alive = nd.zeros(grid_size, nd.Bool)
 	is_alive_inner = is_alive.get(nd.range(1, -1), nd.range(1, -1))
+	image_data = nd.empty_like(is_alive_inner)
 
 	neighour_count = nd.zeros_like(is_alive_inner, nd.Int8)
 	tmp_inner = nd.empty_like(is_alive_inner, nd.Bool)
@@ -31,7 +33,8 @@ func simulation_step() -> void:
 	is_alive_inner.set(false, tmp_inner.assign_greater(neighour_count, 3))
 
 func on_draw() -> void:
-	var image_data := nd.multiply(255, nd.reshape(is_alive, -1)).to_packed_byte_array()
+	image_data.set(is_alive_inner)
+	var image_data := nd.reshape(image_data, -1).to_packed_byte_array()
 	params._image.set_data(params.N, params.N, false, Image.FORMAT_R8, image_data)
 	params.update_texture()
 

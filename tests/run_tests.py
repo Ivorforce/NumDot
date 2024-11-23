c10aa22d8df30763d375601a9544ffad68e9b070
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2024-11-23 23:02:27 +0100

    Automatically determine what tests / benchmarks to run in run_tests.py by asking numpy.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -10,30 +10,18 @@ import os
 import argparse
 import subprocess
 
-class DType(enum.Enum):
-	Int8 = 0
-	Int16 = 1
-	Int32 = 2
-	Int64 = 3
-	UInt8 = 4
-	UInt16 = 5
-	UInt32 = 6
-	UInt64 = 7
-	Float32 = 8
-	Float64 = 9
-	Bool = 10
-	Complex64 = 11
-	Complex128 = 12
-
-dtype_names_np: dict[DType, str] = {
-	dtype: f"np.{dtype.name.lower()}"
-	for dtype in DType
-}
-dtype_names_np[DType.Bool] = "bool"
-
-dtype_names_nd: dict[DType, str] = {
-	dtype: f"nd.{dtype.name}"
-	for dtype in DType
+dtype_names_nd: dict[np.dtype, str] = {
+	np.dtype(np.int8): "Int8",
+	np.dtype(np.int16): "Int16",
+	np.dtype(np.int32): "Int32",
+	np.dtype(np.int64): "Int64",
+	np.dtype(np.uint8): "UInt8",
+	np.dtype(np.uint16): "UInt16",
+	np.dtype(np.uint32): "UInt32",
+	np.dtype(np.uint64): "UInt64",
+	np.dtype(np.bool): "Bool",
+	np.dtype(np.complex64): "Complex64",
+	np.dtype(np.complex128): "Complex128",
 }
 
 @dataclass
@@ -51,7 +39,7 @@ class Arg:
 class Full(Arg):
 	size: int
 	value: str
-	dtype: DType
+	dtype: np.dtype
 
 def as_file_path(string):
 	if os.path.isfile(string):
@@ -59,48 +47,50 @@ def as_file_path(string):
 	else:
 		raise FileNotFoundError(string)
 
-def unary_func_to_gdscript(func, is_complex):
-	if func == "positive":
-		return "{} = +{}"
-	if func == "negative":
-		return "{} = -{}"
-	if func == "square":
-		return "{0} = {1} * {1}"
-	if is_complex and (
-			"sin" in func
-			or "cos" in func
-			or "tan" in func
-			or func == "sqrt"
-	):
-		return f"{{0}} = Vector2({func}({{1}}.x), {func}({{1}}.y))"
-	if is_complex and func == "abs":
-		return f"{{0}} = Vector2(sqrt({{1}}.x * {{1}}.x + {{1}}.y * {{1}}.y), 0)"  # TODO should be set in a float array to be fair
-	return f"{{}} = {func}({{}})"
-
-def binary_func_to_gdscript(func, is_complex):
-	if func == "maximum":
-		func = "max"
-	if func == "minimum":
-		func = "min"
-	if is_complex and (
-			func == "pow"
-			or func == "max"
-			or func == "min"
-	):
-		return f"{{0}} = Vector2({func}({{1}}.x, {{2}}.x), {func}({{1}}.y, {{2}}.y))"
-	if func == "add":
-		return "{} = {} + {}"
-	if func == "subtract":
-		return "{} = {} - {}"
-	if func == "multiply":
-		return "{} = {} * {}"
-	if func == "divide":
-		return "{} = {} / {}"
-	if func == "greater":
-		return "{} = {} > {}"
-	if func == "equal":
-		return "{} = {} == {}"
-	return f"{{}} = {func}({{}}, {{}})"
+def func_to_gdscript(func, nin, is_complex):
+	if nin == 1:
+		if func == "positive":
+			return "{} = +{}"
+		if func == "negative":
+			return "{} = -{}"
+		if func == "square":
+			return "{0} = {1} * {1}"
+		if is_complex and (
+				"sin" in func
+				or "cos" in func
+				or "tan" in func
+				or func == "sqrt"
+		):
+			return f"{{0}} = Vector2({func}({{1}}.x), {func}({{1}}.y))"
+		if is_complex and func == "abs":
+			return f"{{0}} = Vector2(sqrt({{1}}.x * {{1}}.x + {{1}}.y * {{1}}.y), 0)"  # TODO should be set in a float array to be fair
+		return f"{{}} = {func}({{}})"
+	elif nin == 2:
+		if func == "maximum":
+			func = "max"
+		if func == "minimum":
+			func = "min"
+		if is_complex and (
+				func == "pow"
+				or func == "max"
+				or func == "min"
+		):
+			return f"{{0}} = Vector2({func}({{1}}.x, {{2}}.x), {func}({{1}}.y, {{2}}.y))"
+		if func == "add":
+			return "{} = {} + {}"
+		if func == "subtract":
+			return "{} = {} - {}"
+		if func == "multiply":
+			return "{} = {} * {}"
+		if func == "divide":
+			return "{} = {} / {}"
+		if func == "greater":
+			return "{} = {} > {}"
+		if func == "equal":
+			return "{} = {} == {}"
+		return f"{{}} = {func}({{}}, {{}})"
+	else:
+		raise NotImplementedError
 
 
 def make_test_func_np(name, args, stmt):
@@ -128,7 +118,7 @@ func __{name}({args}n):
 def make_np_call(function_name, kwargs: dict[str, Arg], n: int):
 	def arg_to_str(arg: Arg):
 		if isinstance(arg, Full):
-			return f"np.full([{arg.size}], fill_value={arg.value}, dtype={dtype_names_np[arg.dtype]})"
+			return f"np.full([{arg.size}], fill_value={arg.value}, dtype=np.{arg.dtype})"
 		raise Exception()
 
 	args_str = "".join(f'{name}={arg_to_str(value)}, ' for name, value in kwargs.items())
@@ -136,7 +126,7 @@ def make_np_call(function_name, kwargs: dict[str, Arg], n: int):
 
 def nd_arg_to_str(arg: Arg):
 	if isinstance(arg, Full):
-		return f"nd.full([{arg.size}], {arg.value}, {dtype_names_nd[arg.dtype]})"
+		return f"nd.full([{arg.size}], {arg.value}, nd.{dtype_names_nd[arg.dtype]})"
 	raise Exception()
 
 def make_nd_call(function_name, kwargs: dict[str, Arg], n: int):
@@ -144,7 +134,7 @@ def make_nd_call(function_name, kwargs: dict[str, Arg], n: int):
 	return f"\tprint(__{function_name}({args_str}{n}))"
 
 def make_gd_call(function_name, kwargs: dict[str, Arg], n: int):
-	if any(arg.dtype in [DType.Complex64, DType.Complex128] for arg in kwargs.values()):
+	if any(arg.dtype in [np.dtype(np.complex64), np.dtype(np.complex128)] for arg in kwargs.values()):
 		function_name = function_name + "_complex"
 
 	args_str = "".join(f'to_packed({nd_arg_to_str(value)}), ' for name, value in kwargs.items())
@@ -154,7 +144,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--godot', type=as_file_path, required=True, help='Godot binary location')
 
 def main():
-	args = arg_parser.parse_args()
+	cli_args = arg_parser.parse_args()
 
 	test_code_np = \
 """import numpy as np
@@ -203,15 +193,15 @@ func to_packed(array: NDArray):
 		nonlocal test_code_np
 		args_str = "".join(f"{arg}, " for arg in args)
 		test_code_np += make_test_func_np(name, args_str, f"np.{function}({args_str})")
+
 		nonlocal test_code_nd
 		test_code_nd += make_test_func_gd(name, args_str, f"\t\tnd.{function}({args_str})")
 
-	def make_normal_test_func_gd(name, function, replacer, args):
 		nonlocal test_code_gd
 		for is_complex in [True, False]:
 			test_code = "\t\tfor i in x.size():\n\t\t\t"""
 
-			test_code += replacer(function, is_complex).format(*[f"{arg}[i]" for arg in [args[0], *args]])
+			test_code += func_to_gdscript(function, len(args), is_complex).format(*[f"{arg}[i]" for arg in [args[0], *args]])
 
 			args_str_def = "".join(f"{arg}, " for arg in args)
 			test_code_gd += make_test_func_gd(name + ("_complex" if is_complex else ""), args_str_def, test_code)
@@ -225,9 +215,9 @@ func to_packed(array: NDArray):
 		))
 
 	normal_n = 40_000
-	all_dtypes: list[DType] = list(DType)
 
 	for un_function_name in [
+		# Unary
 		# "positive",
 		# "negative",
 		"square",
@@ -238,15 +228,8 @@ func to_packed(array: NDArray):
 		"asin", "acos", "atan",
 		"sinh", "cosh", "tanh",
 		"asinh", "acosh", "atanh",
-	]:
-		append_normal_test_func(un_function_name, un_function_name, "x")
-		make_normal_test_func_gd(un_function_name, un_function_name, unary_func_to_gdscript, "x")
 
-		for dtype in all_dtypes:
-			for s in [50, 1_000, 20000]:
-				append_test(f"{un_function_name}_{dtype.name}_{s}", un_function_name, {"x": Full(s, value="0.5", dtype=dtype)}, n=normal_n // s)
-
-	for bin_function_name in [
+		# Binary
 		"add",
 		"subtract",
 		"multiply",
@@ -257,12 +240,28 @@ func to_packed(array: NDArray):
 		# "minimum",
 		# "maximum",
 	]:
-		append_normal_test_func(bin_function_name, bin_function_name, "xy")
-		make_normal_test_func_gd(bin_function_name, bin_function_name, binary_func_to_gdscript, "xy")
+		np_ufunc = eval(f"np.{un_function_name}")
+		ufunc_args = "xyz"[:np_ufunc.nin]
 
-		for dtype in all_dtypes:
+		append_normal_test_func(un_function_name, un_function_name, ufunc_args)
+
+		for type_str in np_ufunc.types:
+			dtype_in = np.dtype(type_str[0])
+			dtype_out = np.dtype(type_str[-1])
+			if dtype_in != dtype_out:
+				continue  # TODO Our godot test generation can't handle this yet lol
+			if dtype_in not in dtype_names_nd:
+				print(dtype_in)
+				print(list(dtype_names_nd.keys()))
+				continue  # Skip what weNumDot doesn't support anyway.
+
 			for s in [50, 1_000, 20000]:
-				append_test(f"{bin_function_name}_{dtype.name}_{s}", bin_function_name, {"x": Full(s, value="0.5", dtype=dtype), "y": Full(s, value="2", dtype=dtype)}, n=normal_n // s)
+				append_test(
+					f"{un_function_name}_{dtype_in}_{s}",
+					un_function_name,
+					{arg: Full(s, value="0.5", dtype=dtype_in) for arg in ufunc_args},
+					n=normal_n // s
+				)
 
 	py_test_file_path = pathlib.Path(__file__).parent / "gen" / "tests.py"
 	py_test_file_path.parent.mkdir(exist_ok=True)
@@ -285,7 +284,7 @@ func _ready():
 
 		demo_path = pathlib.Path(__file__).parent / ".." / "demo"
 		gd_out = subprocess.check_output(
-			[args.godot, "--path", demo_path, "--headless", "res://tests/run_tests.tscn", "--quit"],
+			[cli_args.godot, "--path", demo_path, "--headless", "res://tests/run_tests.tscn", "--quit"],
 			encoding='UTF-8'
 		)
 		gd_out_lines = gd_out[gd_out.index(start_numdot_tests_string):].split("\n")[1:]

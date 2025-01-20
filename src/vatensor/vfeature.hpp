f2a343d690a0ff5ff412115882840b44468e3813
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-01-20 17:53:08 +0100

    Remove feature tags from the old system that are no longer used.

--- src/vatensor/vfeature.hpp
+++ src/vatensor/vfeature.hpp
@@ -9,28 +9,7 @@ namespace va {
 		linspace,
 		arange,
 
-		bitwise_and,
-		bitwise_or,
-		bitwise_xor,
-		bitwise_not,
-		bitwise_left_shift,
-		bitwise_right_shift,
-
-		logical_and,
-		logical_or,
-		logical_xor,
-		logical_not,
-
-		equal_to,
-		not_equal_to,
-		greater,
-		greater_equal,
-		less,
-		less_equal,
 		is_close,
-		is_nan,
-		is_inf,
-		is_finite,
 		array_equal,
 		all_close,
 
@@ -46,53 +25,15 @@ namespace va {
 		norm_l1,
 		norm_l2,
 		norm_linf,
-		count_nonzero,
 		all,
 		any,
 		reduce_dot,
 
-		ceil,
-		floor,
-		trunc,
 		round,
-		nearbyint,
-
-		sin,
-		cos,
-		tan,
-		asin,
-		acos,
-		atan,
-		atan2,
-		sinh,
-		cosh,
-		tanh,
-		asinh,
-		acosh,
-		atanh,
 
 		negative,
 
-		add,
-		subtract,
-		multiply,
-		divide,
-		remainder,
-		pow,
-
-		minimum,
-		maximum,
 		clip,
-
-		sign,
-		abs,
-		square,
-		sqrt,
-		exp,
-		log,
-
-		rad2deg,
-		deg2rad,
 
 		cross,
 
@@ -117,28 +58,7 @@ namespace va {
 			FEATURE_NAME_CASE(linspace)
 			FEATURE_NAME_CASE(arange)
 
-			FEATURE_NAME_CASE(bitwise_and)
-			FEATURE_NAME_CASE(bitwise_or)
-			FEATURE_NAME_CASE(bitwise_xor)
-			FEATURE_NAME_CASE(bitwise_not)
-			FEATURE_NAME_CASE(bitwise_left_shift)
-			FEATURE_NAME_CASE(bitwise_right_shift)
-
-			FEATURE_NAME_CASE(logical_and)
-			FEATURE_NAME_CASE(logical_or)
-			FEATURE_NAME_CASE(logical_xor)
-			FEATURE_NAME_CASE(logical_not)
-
-			FEATURE_NAME_CASE(equal_to)
-			FEATURE_NAME_CASE(not_equal_to)
-			FEATURE_NAME_CASE(greater)
-			FEATURE_NAME_CASE(greater_equal)
-			FEATURE_NAME_CASE(less)
-			FEATURE_NAME_CASE(less_equal)
 			FEATURE_NAME_CASE(is_close)
-			FEATURE_NAME_CASE(is_nan)
-			FEATURE_NAME_CASE(is_inf)
-			FEATURE_NAME_CASE(is_finite)
 			FEATURE_NAME_CASE(array_equal)
 			FEATURE_NAME_CASE(all_close)
 
@@ -154,53 +74,15 @@ namespace va {
 			FEATURE_NAME_CASE(norm_l1)
 			FEATURE_NAME_CASE(norm_l2)
 			FEATURE_NAME_CASE(norm_linf)
-			FEATURE_NAME_CASE(count_nonzero)
 			FEATURE_NAME_CASE(all)
 			FEATURE_NAME_CASE(any)
 			FEATURE_NAME_CASE(reduce_dot)
 
-			FEATURE_NAME_CASE(ceil)
-			FEATURE_NAME_CASE(floor)
-			FEATURE_NAME_CASE(trunc)
 			FEATURE_NAME_CASE(round)
-			FEATURE_NAME_CASE(nearbyint)
-
-			FEATURE_NAME_CASE(sin)
-			FEATURE_NAME_CASE(cos)
-			FEATURE_NAME_CASE(tan)
-			FEATURE_NAME_CASE(asin)
-			FEATURE_NAME_CASE(acos)
-			FEATURE_NAME_CASE(atan)
-			FEATURE_NAME_CASE(atan2)
-			FEATURE_NAME_CASE(sinh)
-			FEATURE_NAME_CASE(cosh)
-			FEATURE_NAME_CASE(tanh)
-			FEATURE_NAME_CASE(asinh)
-			FEATURE_NAME_CASE(acosh)
-			FEATURE_NAME_CASE(atanh)
 
 			FEATURE_NAME_CASE(negative)
 
-			FEATURE_NAME_CASE(add)
-			FEATURE_NAME_CASE(subtract)
-			FEATURE_NAME_CASE(multiply)
-			FEATURE_NAME_CASE(divide)
-			FEATURE_NAME_CASE(remainder)
-			FEATURE_NAME_CASE(pow)
-
-			FEATURE_NAME_CASE(minimum)
-			FEATURE_NAME_CASE(maximum)
 			FEATURE_NAME_CASE(clip)
-
-			FEATURE_NAME_CASE(sign)
-			FEATURE_NAME_CASE(abs)
-			FEATURE_NAME_CASE(square)
-			FEATURE_NAME_CASE(sqrt)
-			FEATURE_NAME_CASE(exp)
-			FEATURE_NAME_CASE(log)
-
-			FEATURE_NAME_CASE(rad2deg)
-			FEATURE_NAME_CASE(deg2rad)
 
 			FEATURE_NAME_CASE(cross)
 

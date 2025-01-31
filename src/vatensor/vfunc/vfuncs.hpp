4b419452d6bb00966c82795a189e3e4652d094a4
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-01-31 13:56:11 +0100

    Update dtypes of sum() and simplify implementation definitions.

--- src/vatensor/vfunc/vfuncs.hpp
+++ src/vatensor/vfunc/vfuncs.hpp
@@ -6,27 +6,15 @@
 #include "vatensor/vassign.hpp"
 #include "xtensor/xoperation.hpp"
 
-#define IMPLEMENT_BINARY_VFUNC0(UFUNC_NAME, OP)\
+#define IMPLEMENT_BINARY_VFUNC(UFUNC_NAME, OP, ...)\
 template <typename R, typename A, typename B>\
-inline void UFUNC_NAME(R& ret, const A& a, const B& b) {\
+inline void UFUNC_NAME(R& ret, const A& a, const B& b, ##__VA_ARGS__) {\
 	va::broadcasting_assign_typesafe(ret, OP);\
 }
 
-#define IMPLEMENT_BINARY_VFUNC3(UFUNC_NAME, ARG1, ARG2, ARG3, OP)\
-template <typename R, typename A, typename B>\
-inline void UFUNC_NAME(R& ret, const A& a, const B& b, ARG1 arg1, ARG2 arg2, ARG3 arg3) {\
-	va::broadcasting_assign_typesafe(ret, OP);\
-}
-
-#define IMPLEMENT_UNARY_VFUNC0(UFUNC_NAME, OP)\
+#define IMPLEMENT_UNARY_VFUNC(UFUNC_NAME, OP, ...)\
 template <typename R, typename A>\
-inline void UFUNC_NAME(R& ret, const A& a) {\
-	va::broadcasting_assign_typesafe(ret, OP);\
-}
-
-#define IMPLEMENT_UNARY_VFUNC1(UFUNC_NAME, ARG1, OP)\
-template <typename R, typename A>\
-inline void UFUNC_NAME(R& ret, const A& a, ARG1 arg1) {\
+inline void UFUNC_NAME(R& ret, const A& a, ##__VA_ARGS__) {\
 	va::broadcasting_assign_typesafe(ret, OP);\
 }
 
@@ -66,85 +54,87 @@ namespace va::op {
 		template <class T1>
 		constexpr std::decay_t<std::complex<T1>> operator()(const std::complex<T1>& arg1) const { return std::conj(arg1); }
 	};
-
-	template <class T1>
-	constexpr auto sum(T1&& arg1, const va::axes_type* axes) {
-		return axes
-			? xt::sum(std::forward<T1>(arg1), *axes, xt::evaluation_strategy::lazy)
-			: xt::sum(std::forward<T1>(arg1), xt::evaluation_strategy::lazy);
-	}
 }
 
 namespace va::vfunc::impl {
-	IMPLEMENT_UNARY_VFUNC0(negative, -va::promote::to_num(a))
-	IMPLEMENT_UNARY_VFUNC0(sign, xt::sign(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(abs, xt::abs(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(square, xt::square(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(sqrt, xt::sqrt(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(exp, xt::exp(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(log, xt::log(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(rad2deg, xt::rad2deg(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(deg2rad, xt::deg2rad(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(negative, -va::promote::to_num(a))
+	IMPLEMENT_UNARY_VFUNC(sign, xt::sign(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(abs, xt::abs(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(square, xt::square(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(sqrt, xt::sqrt(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(exp, xt::exp(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(log, xt::log(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(rad2deg, xt::rad2deg(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(deg2rad, xt::deg2rad(va::promote::to_num(a)))
 
-	IMPLEMENT_UNARY_VFUNC0(conjugate, xt::detail::make_xfunction<va::op::conj_fun>(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(conjugate, xt::detail::make_xfunction<va::op::conj_fun>(va::promote::to_num(a)))
 
 	// TODO calling xt::add directly etc. is broken because of the DEFINE_COMPLEX_OVERLOAD
-	IMPLEMENT_BINARY_VFUNC0(add, xt::detail::make_xfunction<xt::detail::plus>(va::promote::to_num(a), va::promote::to_num(b)))
-	IMPLEMENT_BINARY_VFUNC0(subtract, xt::detail::make_xfunction<xt::detail::minus>(va::promote::to_num(a), va::promote::to_num(b)));
-	IMPLEMENT_BINARY_VFUNC0(multiply, xt::detail::make_xfunction<xt::detail::multiplies>(va::promote::to_num(a), va::promote::to_num(b)))
-	IMPLEMENT_BINARY_VFUNC0(divide, xt::detail::make_xfunction<xt::detail::divides>(va::promote::to_num(a), va::promote::to_num(b)))
-	IMPLEMENT_BINARY_VFUNC0(remainder, xt::remainder(va::promote::to_num(a), va::promote::to_num(b)))
-	IMPLEMENT_BINARY_VFUNC0(pow, xt::pow(va::promote::to_num(a), va::promote::to_num(b)))
-	IMPLEMENT_BINARY_VFUNC0(minimum, xt::minimum(a, b))
-	IMPLEMENT_BINARY_VFUNC0(maximum, xt::maximum(a, b))
+	IMPLEMENT_BINARY_VFUNC(add, xt::detail::make_xfunction<xt::detail::plus>(va::promote::to_num(a), va::promote::to_num(b)))
+	IMPLEMENT_BINARY_VFUNC(subtract, xt::detail::make_xfunction<xt::detail::minus>(va::promote::to_num(a), va::promote::to_num(b)));
+	IMPLEMENT_BINARY_VFUNC(multiply, xt::detail::make_xfunction<xt::detail::multiplies>(va::promote::to_num(a), va::promote::to_num(b)))
+	IMPLEMENT_BINARY_VFUNC(divide, xt::detail::make_xfunction<xt::detail::divides>(va::promote::to_num(a), va::promote::to_num(b)))
+	IMPLEMENT_BINARY_VFUNC(remainder, xt::remainder(va::promote::to_num(a), va::promote::to_num(b)))
+	IMPLEMENT_BINARY_VFUNC(pow, xt::pow(va::promote::to_num(a), va::promote::to_num(b)))
+	IMPLEMENT_BINARY_VFUNC(minimum, xt::minimum(a, b))
+	IMPLEMENT_BINARY_VFUNC(maximum, xt::maximum(a, b))
 
-	IMPLEMENT_UNARY_VFUNC1(sum, const va::axes_type*, va::op::sum(a, arg1))
+	template <typename R, typename A>
+	inline void sum(R& ret, const A& a, const va::axes_type* axes) {
+		if (axes) {
+			va::broadcasting_assign_typesafe(ret, xt::sum(a, *axes, xt::evaluation_strategy::lazy));
+		}
+		else {
+			const typename R::value_type sum = xt::sum(a)();
+			broadcasting_assign(ret, xt::xscalar<typename R::value_type>(sum));
+		}
+	}
 
-	IMPLEMENT_UNARY_VFUNC0(sin, xt::sin(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(cos, xt::cos(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(tan, xt::tan(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(asin, xt::asin(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(acos, xt::acos(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(atan, xt::atan(va::promote::to_num(a)))
-	IMPLEMENT_BINARY_VFUNC0(atan2, xt::atan2(va::promote::to_num(a), va::promote::to_num(b)))
-	IMPLEMENT_UNARY_VFUNC0(sinh, xt::sinh(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(cosh, xt::cosh(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(tanh, xt::tanh(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(asinh, xt::asinh(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(acosh, xt::acosh(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(atanh, xt::atanh(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(sin, xt::sin(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(cos, xt::cos(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(tan, xt::tan(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(asin, xt::asin(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(acos, xt::acos(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(atan, xt::atan(va::promote::to_num(a)))
+	IMPLEMENT_BINARY_VFUNC(atan2, xt::atan2(va::promote::to_num(a), va::promote::to_num(b)))
+	IMPLEMENT_UNARY_VFUNC(sinh, xt::sinh(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(cosh, xt::cosh(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(tanh, xt::tanh(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(asinh, xt::asinh(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(acosh, xt::acosh(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(atanh, xt::atanh(va::promote::to_num(a)))
 
-	IMPLEMENT_UNARY_VFUNC0(ceil, xt::ceil(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(floor, xt::floor(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(trunc, xt::trunc(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(round, xt::detail::make_xfunction<va::op::round_fun>(a))
+	IMPLEMENT_UNARY_VFUNC(ceil, xt::ceil(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(floor, xt::floor(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(trunc, xt::trunc(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(round, xt::detail::make_xfunction<va::op::round_fun>(a))
 	// Actually uses nearbyint because rint can throw, which is undesirable in our case, and unlike numpy's behavior.
-	IMPLEMENT_UNARY_VFUNC0(rint, xt::nearbyint(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(rint, xt::nearbyint(va::promote::to_num(a)))
 
-	IMPLEMENT_UNARY_VFUNC0(logical_not, !va::promote::to_bool(a))
+	IMPLEMENT_UNARY_VFUNC(logical_not, !va::promote::to_bool(a))
 	// TODO RE-optimize these to short circuit on scalars
-	IMPLEMENT_BINARY_VFUNC0(logical_and, va::promote::to_bool(a) && va::promote::to_bool(b))
-	IMPLEMENT_BINARY_VFUNC0(logical_or, va::promote::to_bool(a) || va::promote::to_bool(b))
-	IMPLEMENT_BINARY_VFUNC0(logical_xor, xt::detail::make_xfunction<xt::detail::not_equal_to>(va::promote::to_bool(a), va::promote::to_bool(b)))
+	IMPLEMENT_BINARY_VFUNC(logical_and, va::promote::to_bool(a) && va::promote::to_bool(b))
+	IMPLEMENT_BINARY_VFUNC(logical_or, va::promote::to_bool(a) || va::promote::to_bool(b))
+	IMPLEMENT_BINARY_VFUNC(logical_xor, xt::detail::make_xfunction<xt::detail::not_equal_to>(va::promote::to_bool(a), va::promote::to_bool(b)))
 
-	IMPLEMENT_UNARY_VFUNC0(bitwise_not, xt::detail::make_xfunction<va::op::bitwise_not_boolsafe>(a))
-	IMPLEMENT_BINARY_VFUNC0(bitwise_and, a & b)
-	IMPLEMENT_BINARY_VFUNC0(bitwise_or, a | b)
-	IMPLEMENT_BINARY_VFUNC0(bitwise_xor, a ^ b)
-	IMPLEMENT_BINARY_VFUNC0(bitwise_left_shift, xt::detail::make_xfunction<va::op::left_shift_safe>(a, b))
-	IMPLEMENT_BINARY_VFUNC0(bitwise_right_shift, xt::detail::make_xfunction<va::op::right_shift_safe>(a, b))
+	IMPLEMENT_UNARY_VFUNC(bitwise_not, xt::detail::make_xfunction<va::op::bitwise_not_boolsafe>(a))
+	IMPLEMENT_BINARY_VFUNC(bitwise_and, a & b)
+	IMPLEMENT_BINARY_VFUNC(bitwise_or, a | b)
+	IMPLEMENT_BINARY_VFUNC(bitwise_xor, a ^ b)
+	IMPLEMENT_BINARY_VFUNC(bitwise_left_shift, xt::detail::make_xfunction<va::op::left_shift_safe>(a, b))
+	IMPLEMENT_BINARY_VFUNC(bitwise_right_shift, xt::detail::make_xfunction<va::op::right_shift_safe>(a, b))
 
-	IMPLEMENT_BINARY_VFUNC0(equal, xt::equal(a, b))
-	IMPLEMENT_BINARY_VFUNC0(not_equal, xt::not_equal(a, b))
-	IMPLEMENT_BINARY_VFUNC0(less, xt::less(a, b))
-	IMPLEMENT_BINARY_VFUNC0(less_equal, xt::less_equal(a, b))
-	IMPLEMENT_BINARY_VFUNC0(greater, xt::greater(a, b))
-	IMPLEMENT_BINARY_VFUNC0(greater_equal, xt::greater_equal(a, b))
+	IMPLEMENT_BINARY_VFUNC(equal, xt::equal(a, b))
+	IMPLEMENT_BINARY_VFUNC(not_equal, xt::not_equal(a, b))
+	IMPLEMENT_BINARY_VFUNC(less, xt::less(a, b))
+	IMPLEMENT_BINARY_VFUNC(less_equal, xt::less_equal(a, b))
+	IMPLEMENT_BINARY_VFUNC(greater, xt::greater(a, b))
+	IMPLEMENT_BINARY_VFUNC(greater_equal, xt::greater_equal(a, b))
 
-	IMPLEMENT_UNARY_VFUNC0(isnan, xt::isnan(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(isfinite, xt::isfinite(va::promote::to_num(a)))
-	IMPLEMENT_UNARY_VFUNC0(isinf, xt::isinf(va::promote::to_num(a)))
-	IMPLEMENT_BINARY_VFUNC3(is_close, double, double, bool, xt::isclose(va::promote::to_num(a), arg1, arg2, arg3))
+	IMPLEMENT_UNARY_VFUNC(isnan, xt::isnan(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(isfinite, xt::isfinite(va::promote::to_num(a)))
+	IMPLEMENT_UNARY_VFUNC(isinf, xt::isinf(va::promote::to_num(a)))
+	IMPLEMENT_BINARY_VFUNC(is_close, xt::isclose(va::promote::to_num(a), va::promote::to_num(b), rtol, atol, equal_nan), double rtol, double atol, bool equal_nan)
 } // namespace va::vfunc::impl
 
 #endif //VFUNCS_HPP

2b96e1ada011353d23d12edfb708f64e7bae0348
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-01-31 18:31:41 +0100

    Remove now obsolete boilerplate from vcompute.

--- src/vatensor/vcompute.hpp
+++ src/vatensor/vcompute.hpp
@@ -1,9 +1,6 @@
 #ifndef VCOMPUTE_INPLACE_H
 #define VCOMPUTE_INPLACE_H
 
-#include <cmath>                       // for double_t
-#include <cstdint>                     // for int64_t
-#include <type_traits>                  // for decay_t, conditional_t, disju...
 #include <utility>                      // for forward
 #include <variant>                      // for visit, variant
 #include <vector>                       // for vector
@@ -12,8 +9,6 @@
 #include "vpromote.hpp"                   // for promote_value_type_if_needed
 #include "vconfig.hpp"
 #include "dtype.hpp"
-#include "xtensor/xarray.hpp"           // for xarray_container
-#include "xtensor/xoperation.hpp"       // for xfunction_type_t
 #include "xtensor/xstorage.hpp"         // for uvector
 #include "xtensor/xtensor_forward.hpp"  // for xarray
 
@@ -56,112 +51,6 @@ namespace va {
                 std::move(data),
                 0
             }
-        );
-    }
-
-    template<typename OutputType, typename Result>
-    void assign_to_target(const VArrayTarget& target, VStoreAllocator& allocator, const Result& result) {
-        // Some functions (or compilers) may offer 128 bit types as results from functions.
-        // We may not be able to store them. This is not the best way to go about it
-        // (as incompatible types COULD be less than the supported ones, prompting us to upcast), but it's good enough for now.
-        using RNatural = typename std::decay_t<decltype(result)>::value_type;
-        using RStorable = promote::compatible_type_or_64_bit_t<RNatural, VScalar>;
-
-        std::visit(
-            [&result, &allocator](auto& target) {
-                using PtrType = std::decay_t<decltype(target)>;
-
-#ifndef NUMDOT_COPY_FOR_ALL_INPLACE_OPERATIONS
-                if constexpr (std::is_same_v<PtrType, VData*>) {
-                    // Assign to compute case, broadcasting and casting if necessary.
-                    if (std::visit(
-                        [&result](auto& ctarget) {
-                            using T = typename std::decay_t<decltype(ctarget)>::value_type;
-
-#ifndef NUMDOT_OPTIMIZE_ALL_INPLACE_OPERATIONS
-                            // If the target type is the same as the natural operation type,
-                            // we can just assign. It's just one operation, and it's the most important one!
-                            // Otherwise, we may want to avoid it because it generates a lot of code (nxm).
-                            if constexpr (!std::is_same_v<T, RNatural>) {
-                                // We need to cast, just give up here.
-                                return false;
-                            }
-                            else
-#endif
-                            {
-                                // TODO Could use assign_xexpression if there is no aliasing, aka overlap of target and inputs.
-                                va::broadcasting_assign(ctarget, result);
-                                return true;
-                            }
-                        }, *target
-                    )) {
-                        // Ran accelerated assign, we don't need to do the regular one.
-                        return;
-                    }
-                    else
-#endif
-                    {
-                        if constexpr (!std::is_convertible_v<RNatural, RStorable>) {
-                            throw std::runtime_error("Cannot store the function result in an array of this dtype.");
-                        }
-                        else {
-                            // Make a copy, similar as in promote_compute_case_if_needed.
-                            // This is the same operation on all branches so we don't produce much code.
-                            const auto temporary = create_varray<RStorable>(allocator, result);
-                            // Now, a normal type conversion.
-                            va::assign(*target, temporary->data);
-                        }
-                    }
-                }
-                else {
-                    *target = create_varray<OutputType>(allocator, result);
-                }
-            }, target
-        );
-    }
-
-    // This function mostly exists to make it easier for the compiler to de-duplicate code.
-    template<typename PromotionRule, typename FX, typename... Args>
-    static void vfunction_monotype(const FX& fx, VStoreAllocator& allocator, const VArrayTarget& target, const Args&... args) {
-        using InputType = promote::value_type_v<std::tuple_element_t<0, std::tuple<std::decay_t<Args>...>>>;
-        static_assert(
-            (std::is_same_v<promote::value_type_v<std::decay_t<Args>>, InputType> && ...),
-            "All value types in monotype must be the same."
-        );
-
-        // Result of visitor invocation
-        const auto result = fx(args...);
-
-        using NaturalOutputType = typename std::decay_t<decltype(result)>::value_type;
-        using OutputType = typename PromotionRule::template output_type<InputType, NaturalOutputType>;
-
-        assign_to_target<OutputType>(target, allocator, result);
-    }
-
-    template<Feature feature, typename PromotionRule, typename FX, typename Arg>
-    static void xoperation_single(const FX& fx, VStoreAllocator& allocator, const VArrayTarget& target, const Arg arg) {
-        visit_if_enabled<feature>(
-            [&fx, &allocator, &target](const auto& arg) {
-                using InputType = typename PromotionRule::template input_type<promote::value_type_v<std::decay_t<decltype(arg)>>>;
-
-                if constexpr (std::is_same_v<InputType, void>) {
-                    throw std::runtime_error("Unsupported type for operation.");
-                }
-                else if constexpr (!std::disjunction_v<std::is_convertible<promote::value_type_v<std::decay_t<decltype(arg)>>, InputType>>) {
-                    throw std::runtime_error("Cannot promote in this way.");
-                }
-                else {
-                    vfunction_monotype<PromotionRule>(
-                        fx,
-                        allocator,
-                        target,
-                        promote::deref_promoted<InputType>(
-                            promote::deref_data(promote::promote_value_type_if_needed<InputType>(arg))
-                        )
-                    );
-                }
-            },
-            arg
         );
     }
 }

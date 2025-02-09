4aad9e4e4bc7cb08bab795cb9cc69871bf0a903c
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-02-09 21:25:41 +0100

    Update godot-cpp, remove (probably) unneeded build_profile.json members.

--- demo/tests.gd
+++ demo/tests.gd
@@ -3,4 +3,5 @@ extends Node2D
 func _ready() -> void:
 	# Edit your testing code here
 	# But don't submit the changes to the repository!
-	print(nd.full([100], 2, nd.Bool).to_packed_byte_array())
+	print(nd.arange(0, 1000, 1, nd.Int64))
+	#print(nd.bitwise_left_shift(nd.array(2, nd.Int8), nd.array(-1, nd.Int8)))

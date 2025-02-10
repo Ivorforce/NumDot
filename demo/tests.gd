8d6978bc70a1ca8847899a7df948ac8b8beee074
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-02-10 16:36:30 +0100

    Remove files that should not be tracked (again... I need a better solution for this.).

--- demo/tests.gd
+++ demo/tests.gd
@@ -3,5 +3,4 @@ extends Node2D
 func _ready() -> void:
 	# Edit your testing code here
 	# But don't submit the changes to the repository!
-	print(nd.arange(0, 1000, 1, nd.Int64))
-	#print(nd.bitwise_left_shift(nd.array(2, nd.Int8), nd.array(-1, nd.Int8)))
+	pass
